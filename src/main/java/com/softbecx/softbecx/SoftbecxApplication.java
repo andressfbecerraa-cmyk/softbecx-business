00d95eea34685d88e950900508d6b67a203a53f2
andressfbecerraa-cmyk <andressf.becerraa@gmail.com>
2026-09-20 17:22:38 -0500

    Initial commit - Proyecto SOFTBECX

---
+++ src/main/java/com/softbecx/softbecx/SoftbecxApplication.java
@@ -0,0 +1,13 @@
+package com.softbecx.softbecx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftbecxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftbecxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softbecx/softbecx/SoftbecxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softbecx.softbecx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftbecxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
